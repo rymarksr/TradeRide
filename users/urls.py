b320131ba9d005bb42d2eb14409082b62f68d88a
Reemark Recaplaza <87219469+ree-sr@users.noreply.github.com>
2023-12-13 17:35:09 +0800

    Initial commit

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import login_view, logout_view, RegisterView, ProfileView
+
+urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+
+]
